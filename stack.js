a756dbc73ba0a45c7ae950d5219c9b0cce8bd7c7
Saif <saifisami2005@gmail.com>
2024-09-17 15:52:12 -0400

    Finished the implementation of stacks.

--- stack.js
+++ stack.js
@@ -20,26 +20,38 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node;
+    newNode.val = val;
+    if(!this.first) this.first = this.last = newNode;
+    else{
+      let oldFirst = this.first
+      this.first = newNode;
+      this.first.next = oldFirst;
+    }
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    let valueToBeReturned = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    return valueToBeReturned;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size) return false;
+    return true;
   }
 }
 
