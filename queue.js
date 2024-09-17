f473983dbb09f14ee67cb809cfd75e3025913732
Saif <saifisami2005@gmail.com>
2024-09-17 14:36:38 -0400

    Finished the implementation of queues.

--- queue.js
+++ queue.js
@@ -20,26 +20,34 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node;
+    newNode.val = val;
+    if (!this.first) this.first = this.last = newNode;
+    else this.last.next = this.last = newNode;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    let valueToBeReturned = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    return valueToBeReturned;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size) return false;
+    return true;
   }
 }
 
